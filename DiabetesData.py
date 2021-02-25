a9429e42dcc97cfbb77d3468286720b469acbd6a
JoseBuitrago <jbuitrago29@uan.edu.co>
2021-02-25 10:57:38 -0500

    Avance02_2021/02/25*****preprocessing LabelEncoder

--- DiabetesData.py
+++ DiabetesData.py
@@ -1,5 +1,5 @@
 """
-Spyder Editor
+PyCharm
 Creado sábado, 13 de febrero de 2021
 
 @author: José Alejandro Buitrago Cardenas / Leydi Esperanza Perez Leal
@@ -14,54 +14,29 @@ from sklearn.metrics import classification_report, confusion_matrix
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 #Subir dataframe
-
 print ("************************************************************************")
 
 #Read dataset to pandas dataframe
-diabetesdata = pd.read_csv(r'C:\Users\Usuario\Downloads\diabetes_data_upload.csv')
-print(diabetesdata.head())
+df = pd.read_csv(r'C:\Users\Usuario\Downloads\diabetes_data_upload.csv')
+print(df.head())
 print ("************************************************************************")
 
-diabetesdata.rename(columns={'class': 'Class',
-                           'Class': 'class'}, inplace=True)
-print(diabetesdata.head())
+#Crear una función para LabelEncoder
+def Encoder(df):
+#crear un objeto 'columnsToEncode' que hará una lista de columnas que tienen valores categóricos
+    columnsToEncode = list(df.select_dtypes(include=['category', 'object']))
+    le = LabelEncoder()
+#recorrer en iteración las columnas de la lista 'columnsToEncode'
+    for feature in columnsToEncode:
+        try:
+            df[feature] = le.fit_transform(df[feature])
+        except:
+            print('Error encoding ' + feature)
+    return df
 
-# Assign data from first four columns to X variable
-X = diabetesdata.iloc[:, 0:1]
-print(X.head())
-# Assign data from first fifth columns to y variable
-y = diabetesdata.select_dtypes(include=[object])
-print(y.head())
-
-y.Class.unique()
-
-le = preprocessing.LabelEncoder()
-
-y = y.apply(le.fit_transform)
-print(y.head())
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20)
-
-
-scaler = StandardScaler()
-scaler.fit(X_train)
-
-
-X_train = scaler.transform(X_train)
-X_test = scaler.transform(X_test)
-mlp = MLPClassifier(hidden_layer_sizes=(10, 10, 10), max_iter=100)
-print(X.shape)
-y.shape
-print(y.shape)
-
-"""
-mlp.fit(X_train, y_train.values.ravel())
-
-
-predictions = mlp.predict(X_test)
-
-print(confusion_matrix(y_test,predictions))
-print(classification_report(y_test,predictions))
-"""
+columnsToEncode = list(df.select_dtypes(include=['category','object']))
+#pasar marco de datos a través de la función
+df = Encoder(df)
+#ver conjunto de datos
+print(df)
